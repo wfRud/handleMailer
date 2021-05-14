e9fa571dc776711c22de4031d29bd08f7fd120f7
Filip Rudek <rudek.filip@gmail.com>
2021-05-14 12:55:11 +0200

    add handleForm

---
+++ handleForm.js
@@ -0,0 +1,32 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+const contactAddress = "robertpala77@gmail.com";
+const mailer = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: process.env.GMAIL_ADDRESS,
+    pass: process.env.GMAIL_PASSWORD,
+  },
+});
+
+app.post("/contact", function (req, res) {
+  mailer.sendMail(
+    {
+      from: req.body.from,
+      to: [contactAddress],
+      subject: req.body.subject || "[No subject]",
+      html: req.body.message || "[No message]",
+    },
+    function (err, info) {
+      if (err) return res.status(500).send(err);
+      res.json({ success: true });
+      console.log("mail wysłany");
+    }
+  );
+});
+app.listen(3000, () => console.log("serwer działa"));
